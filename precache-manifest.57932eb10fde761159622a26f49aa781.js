fc27ce7bb2fd470a524c12b961738461fc46a53a
Henry Salim <henrysalim31@gmail.com>
2020-09-09 10:32:21 +0300

    Updates

---
+++ precache-manifest.57932eb10fde761159622a26f49aa781.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775b807339bcfa2b3197260c46212676",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "73ef31b8c5c35717a237",
+    "url": "/Monsters/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5912530f344a66580673",
+    "url": "/Monsters/static/css/main.0fbffc55.chunk.css"
+  },
+  {
+    "revision": "73ef31b8c5c35717a237",
+    "url": "/Monsters/static/js/2.557e44d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.557e44d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5912530f344a66580673",
+    "url": "/Monsters/static/js/main.9dbcac74.chunk.js"
+  },
+  {
+    "revision": "5c308a2c8e440c4fc5b5",
+    "url": "/Monsters/static/js/runtime-main.8d610a66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
